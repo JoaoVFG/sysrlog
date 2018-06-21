449e982652ba361306dc6bab3d027c513e83bd38
Joao Vitor <=>
2018-06-21 12:09:16 -0300

    Projeto Criado

---
+++ src/main/java/JoaoVFG/com/github/TgSysrlogApplication.java
@@ -0,0 +1,12 @@
+package JoaoVFG.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgSysrlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgSysrlogApplication.class, args);
+	}
+}
