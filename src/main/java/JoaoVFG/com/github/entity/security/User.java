d5ca74dcfbcaf01951d23c81d7a99005d04f5693
Joao Vitor <=>
2018-08-09 12:04:31 -0300

    Cors liberado

--- src/main/java/JoaoVFG/com/github/entity/security/User.java
+++ src/main/java/JoaoVFG/com/github/entity/security/User.java
@@ -3,7 +3,6 @@ package JoaoVFG.com.github.entity.security;
 import java.io.Serializable;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
