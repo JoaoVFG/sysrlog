6b8d1febb40dee822e707a6b4a9be249da3700fa
Joao Vitor F Garcia <joao.vfg@hotmail.com.br>
2018-09-16 12:18:26 -0300

    123 - Adicionado Método findByAPIKEY em User Repository | Alterado atributo apikey na Entity User

--- src/main/java/JoaoVFG/com/github/repositories/security/UserRepository.java
+++ src/main/java/JoaoVFG/com/github/repositories/security/UserRepository.java
@@ -23,4 +23,7 @@ public interface UserRepository extends JpaRepository<User, Integer>{
 	@Transactional(readOnly = true)
 	@Query("SELECT user FROM User user,Funcionario funcionario WHERE funcionario.empresa.id = :idEmpresa and funcionario.pessoa.id = user.pessoa.id")
 	public List<User> findUsersByEmpresa(@Param("idEmpresa")Integer idEmpresa);
+	
+	@Transactional(readOnly = true)
+	public User findByapiKey(String apiKey);
 }
